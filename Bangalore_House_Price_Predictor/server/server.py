81678ffe7492be91f1b3f36d70c7d8c2df91ecad
farhathulAfrah <afrah@agentz.ai>
2025-06-26 21:16:38 +0530

    BHP

--- Bangalore_House_Price_Predictor/server/server.py
+++ Bangalore_House_Price_Predictor/server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import util
+import os
 
 app = Flask(__name__)
 
@@ -37,4 +38,5 @@ def predict_home_price():
 
 if __name__ == '__main__':
     print("Starting Flask server for Banglore House Price Predictor....")
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
